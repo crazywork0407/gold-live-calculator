77776ad4cb55dab58ce4ed883afdb36a0ab33d85
Andy Cenoz <crazywork0407@gmail.com>
2023-01-28 14:19:13 +0100

    Init

---
+++ gold-live-calculator.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Gold.live Calculator
+ * Plugin URI: https://github.com/WordPress/gutenberg
+ * Description: Calculator plugin for Gold.live
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ * Version: 1.0.0
+ * Author: Eugene Lee
+ * Text Domain: gold-live-calculator
+ *
+ * @package gold.live
+ */
